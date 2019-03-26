1c4eb89dfef6a073eba22e53baa7164cec47d29a
JoseLuisCastilla <48228633+JoseLuisCastilla@users.noreply.github.com>
2019-03-26 22:43:36 +0100

    Prueba para Git Hub
    Prueba para comprobar si sé manejar el Git Hub.

---
+++ PruebaParaGitHub2/src/paquete/pruebaparagithub2/PruebaParaGitHub.java
@@ -0,0 +1,12 @@
+package paquete.pruebaparagithub2;
+
+public class PruebaParaGitHub {
+
+	public static void main(String[] args) {
+		String variablePrueba = "ppqq";
+		variablePrueba = "Prueba para Git Hub";
+		System.out.println(variablePrueba);
+
+	}
+
+}
